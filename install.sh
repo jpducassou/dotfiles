c7b47e76a5d838aaa1e2a0423f216399ff25b07d
Jean Pierre Ducassou <ducassou@ort.edu.uy>
2014-03-07 10:23:14 -0200

    Added target directory creation to install script

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ function linkall() {
 	local SRC_DIR=$1
 	local DES_DIR=$2
 
+	mkdir --parents "$HOME/$DES_DIR"
+
 	for file in $(find "$PWD/$SRC_DIR" -mindepth 1 -maxdepth 1)
 	do
 			ln --symbolic --verbose --force --target-directory="$HOME/$DES_DIR" "$file"
