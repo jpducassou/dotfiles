a3e5ff9ae892d2e437b037094f41318e47d7cd9e
Jean Pierre Ducassou <jpducassou@gmail.com>
2016-03-25 02:49:57 -0300

    Cleanup.

--- install.sh
+++ install.sh
@@ -7,31 +7,31 @@ PWD=$(pwd)
 
 function linkall() {
 
-	local SRC_DIR=$1
-	local DES_DIR=$2
+	local SRC_DIR="${1}"
+	local DES_DIR="${2}"
 
-	mkdir -p "$HOME/$DES_DIR"
+	mkdir -p "${HOME}/${DES_DIR}"
 
-	for file in $(find "$SRC_DIR" -mindepth 1 -maxdepth 1)
+	for file in $(find "${SRC_DIR}" -mindepth 1 -maxdepth 1)
 	do
-		basename=$(basename $file)
-		if [ -e "$HOME/$DES_DIR/$basename" ]; then
+		basename=$(basename "${file}")
+		if [ -e "${HOME}/${DES_DIR}/${basename}" ]; then
 			link=$(readlink "$HOME/$DES_DIR/$basename")
-			if [ ! "$PWD/$file" == "$link" ]; then
-				echo "Must delete '$HOME/$DES_DIR/$basename' !"
+			if [ ! "${PWD}/${file}" == "${link}" ]; then
+				echo "Must delete '${HOME}/${DES_DIR}/${basename}' !"
 			else
-				echo "[OK] $DES_DIR/$basename"
+				echo "[OK] ${DES_DIR}/${basename}"
 			fi
 		else
-			ln -vs "$PWD/$file" "$HOME/$DES_DIR"
+			ln -vs "${PWD}/${file}" "${HOME}/${DES_DIR}"
 		fi
 	done
 
-	for file in $(find "$HOME/$DES_DIR" -mindepth 1 -maxdepth 1 -type l -xtype l)
+	for file in $(find "${HOME}/${DES_DIR}" -mindepth 1 -maxdepth 1 -type l -xtype l)
 	do
-		basename=$(basename $file)
-		echo "[INFO] deleting broken link '$basename'"
-		rm "$HOME/$DES_DIR/$basename"
+		basename=$(basename "${file}")
+		echo "[INFO] deleting broken link '${basename}'"
+		rm "${HOME}/${DES_DIR}/${basename}"
 	done
 
 }
@@ -39,12 +39,12 @@ function linkall() {
 function install_hooks() {
 
 	hook_names=(post-checkout post-merge)
-	script_path="../../$(basename $0)"
+	script_path="../../$(basename ${0})"
 
 	for hook in "${hook_names[@]}"; do
-		if [ ! -h ".git/hooks/$hook" ]; then
-			echo "[INFO] linking .git/hooks/$hook -> $0"
-			ln -vs "$script_path" ".git/hooks/$hook"
+		if [ ! -h ".git/hooks/${hook}" ]; then
+			echo "[INFO] linking .git/hooks/${hook} -> ${0}"
+			ln -vs "${script_path}" ".git/hooks/${hook}"
 		fi
 	done
 
