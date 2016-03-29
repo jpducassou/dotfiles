3a27016fa48982dc8b6e95ac17d615dec32d8761
Jean Pierre Ducassou <jpducassou@gmail.com>
2016-03-28 21:25:48 -0300

    Added local declaration for function variables.

--- install.sh
+++ install.sh
@@ -9,14 +9,15 @@ function linkall() {
 
 	local src_dir="${1}"
 	local des_dir="${2}"
+	local file
 
 	mkdir -p "${HOME}/${des_dir}"
 
 	for file in $(find "${src_dir}" -mindepth 1 -maxdepth 1)
 	do
-		basename=$(basename "${file}")
+		local basename=$(basename "${file}")
 		if [ -e "${HOME}/${des_dir}/${basename}" ]; then
-			link=$(readlink "${HOME}/${des_dir}/$basename")
+			local link=$(readlink "${HOME}/${des_dir}/$basename")
 			if [ ! "${PWD}/${file}" == "${link}" ]; then
 				echo "Must delete '${HOME}/${des_dir}/${basename}' !"
 			else
@@ -29,7 +30,7 @@ function linkall() {
 
 	for file in $(find "${HOME}/${des_dir}" -mindepth 1 -maxdepth 1 -type l -xtype l)
 	do
-		basename=$(basename "${file}")
+		local basename=$(basename "${file}")
 		echo "[INFO] deleting broken link '${basename}'"
 		rm "${HOME}/${des_dir}/${basename}"
 	done
@@ -38,12 +39,12 @@ function linkall() {
 
 function install_hooks() {
 
-	hook_names=(post-checkout post-merge)
-	script_basename=$(basename "${0}")
-	script_path="../../${script_basename}"
+	local hook_names=(post-checkout post-merge)
+	local script_basename=$(basename "${0}")
+	local script_path="../../${script_basename}"
 
 	for hook_name in "${hook_names[@]}"; do
-		hook_path=".git/hooks/${hook_name}"
+		local hook_path=".git/hooks/${hook_name}"
 		if [ ! -h "${hook_path}" ]; then
 			echo "[INFO] linking ${hook_path} -> ${0}"
 			ln -vs "${script_path}" "${hook_path}"
