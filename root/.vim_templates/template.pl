dfb90063acf71aa7d8c616a4559ad32eb32298d1
Jean Pierre Ducassou <(none)>
2017-07-09 15:49:53 -0300

    Added command line param parsing and usage to tpl

--- root/.vim_templates/template.pl
+++ root/.vim_templates/template.pl
@@ -3,14 +3,55 @@
 # ============================================================================
 use strict;
 use warnings;
+use utf8;
 
 # ============================================================================
+use Data::Dumper;
+use Try::Tiny;
+use Getopt::Long;
+use Pod::Usage;
+
+# ============================================================================
+our $VERSION = '0.1';
+my $VERBOSE = 0;
+my $DEBUG   = 0;
 
 # ============================================================================
 main(@ARGV) unless caller();
 
 # ============================================================================
+=pod
+
+=head1 NAME
+
+<FILENAME>
+
+=head1 SYNOPSIS
+
+<FILENAME> --help
+
+=head1 OPTIONS
+
+=item B<help, h>
+
+Print a brief help message and exits.
+
+=head1 AUTHOR
+
+<AUTHOR> - <EMAIL>
+
+=head1 COPYRIGHT AND DISCLAIMER
+
+=cut
+
+# ============================================================================
 sub main {
+
+	GetOptions(
+		'help|h'	   => sub { pod2usage(0); },
+		'verbose|v+' => \$VERBOSE,
+		'debug|d'	   => sub { $VERBOSE = 1; $DEBUG = 1; }
+	) or pod2usage(2);
 
 	# implementation here
 	return;
