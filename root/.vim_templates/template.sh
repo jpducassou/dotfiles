c5b9c2439a60641cce19e636fde0999f58a3af0b
Jean Pierre Ducassou <jpducassou@gmail.com>
2017-10-15 20:34:33 -0300

    Improved logging for bash

--- root/.vim_templates/template.sh
+++ root/.vim_templates/template.sh
@@ -4,7 +4,6 @@
 # Log auxiliary functions
 # ============================================================================
 escape="\033["
-reset="${escape}0m"
 
 # Colors
 green="32"
@@ -12,39 +11,39 @@ yellow="33"
 red="31"
 
 function info() {
-	message="${1}"
+	local message="${1}"
 	_log "INFO" "${green}" "${message}"
 }
 
 function warn() {
-	message="${1}"
+	local message="${1}"
 	_log "WARN" "${yellow}" "${message}"
 }
 
 function error() {
-	message="${1}"
+	local message="${1}"
 	_log "ERROR" "${red}" "${message}"
 }
 
 # USAGE: log "info" "message"
 function log() {
-	level="${1}"
-	message="${2}"
+	local level="${1}"
+	local message="${2}"
 
-	$1 "${message}"
+	${level} "${message}"
 
 }
 
 # USAGE: _log "info" "32" "message"
 function _log() {
-	level="${1}"
+	local level="${1}"
+	local color=""
+	local reset=""
 	if [ -t 2 ]; then
 		color="${escape}0;${2}m"
-	else
-		color=""
-		reset=""
+		reset="${escape}0m"
 	fi
-	message="${3}"
+	local message="${3}"
 
 	>&2 echo -e "${color}[${level}]${reset}\t${message}"
 
