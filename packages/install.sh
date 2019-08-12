29f0a1e6e7c53ec028e2a08861f2d9bf8e3a9fd7
Jean Pierre Ducassou <jpducassou@gmail.com>
2019-08-12 16:12:05 -0300

    Added a few libraries

--- packages/install.sh
+++ packages/install.sh
@@ -10,5 +10,6 @@ if [ "$#" == "0" ]; then
 	echo 'Usage ./install.sh <file_with_list_of_packages>'
 	exit 1
 fi
-grep -vh '#' $@ | xargs aptitude install -y
+
+grep -vh '#' $@ | xargs aptitude install
 
