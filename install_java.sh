f6603d8a5f2a66640726990f7a69938ff4a6ca12
Jean Pierre Ducassou <jpducassou@gmail.com>
2025-07-30 22:01:05 -0300

    Added script for intalling java sdks.

---
+++ install_java.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -d "${HOME}/.sdkman" ]; then
+	echo "[info] Installing SDKMAN ..."
+	curl -s 'https://get.sdkman.io?ci=true&rcupdate=false' | bash
+fi
+source "${HOME}/.sdkman/bin/sdkman-init.sh"
+
+echo "[info] Installing Java SDK 17 with SDKMAN ..."
+sdk install java '17.0.16-amzn'
+JAVA_17_DIR=$(sdk home java '17.0.16-amzn')
+
+echo "[info] Installing Java SDK 21 with SDKMAN ..."
+sdk install java '21.0.8-amzn'
+JAVA_21_DIR=$(sdk home java '21.0.8-amzn')
+
+if [ ! -d "${HOME}/.jenv" ]; then
+	echo "[info] Installing jenv ..."
+	git clone https://github.com/jenv/jenv.git ~/.jenv
+fi
+
+# Load jenv to environment (can copy the folling to the bashrc file)
+export PATH="${HOME}/.jenv/bin:${PATH}"
+eval "$(jenv init -)"
+
+echo "[info] Addding Java SDK 17 to jenv ..."
+jenv add "${JAVA_17_DIR}"
+
+echo "[info] Addding Java SDK 21 to jenv ..."
+jenv add "${JAVA_21_DIR}"
+
+echo "[info] Adding gradle plugin to jenv ..."
+jenv enable-plugin export
+jenv enable-plugin gradle
+
+echo "[info] setting Java SDK 21 as default ..."
+jenv global 21
+
+echo ""
+echo "[info] Done."
+echo "[info] Use 'jenv global <version>' or 'jenv local <version>' to change sdk."
+echo "[info] To uninstall just rm -rf ${HOME}/.sdkman && ${HOME}/.jenv."
+
